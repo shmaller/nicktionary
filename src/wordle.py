62737c2b4ea4b77a358d6df2b9ab63125675df0a
Nicholas Boni <nick@nicholasboni.com>
2023-12-13 13:47:15 -1000

    remove debug prints

--- src/wordle.py
+++ src/wordle.py
@@ -16,73 +16,51 @@ def resource_path(relative_path):
 
 def evaluate_guess(guess,answer):
     '''
-    There is still a bug where, if a guess has duplicate letters,
-    one of which starts the guess,
-    one of which exists later in the solution,
-    it will leave the first result as '.' in the outstr
+    Checks each letter of guess against all letters in answer.
+    Constructs outstr containing '-','X','O' if guess letter is
+    missing, in wrong place, or correct.
+
+    Returns outstr.
     '''
     outstr = '.....'
     guess_i = 0
     while guess_i < len(guess):
         action_taken = False
         guess_letter = guess[guess_i]
-        # print(f'Guess letter: {guess_letter}')
         answer_i = 0
         while answer_i < len(answer):
             answer_letter = answer[answer_i]
-            # print(f'Answer letter: {answer_letter}')
             if guess_letter == answer_letter:
-#                     print(f'Guess letter {guess_letter} at guess position {guess_i} \
-# # matches answer letter {answer_letter} at answer position {answer_i}. Investigating...')
                     if guess_i == answer_i:
                         action_taken = True
                         outstr = splice_str(outstr,'O',guess_i)
                         guess = splice_str(guess,'_',guess_i)
                         answer = splice_str(answer,'_',guess_i)
-#                         print(f'O result. outstr now looks like this: {outstr}\
-# guess now looks like this: {guess}\
-# answer now looks like this: {answer}')
                         break
                     else:
-                        # print('The positions do not match. Not an O.')
-                        # print('Checking for double letters in guess.')
                         future_guess_letters = guess[guess_i+1:]
-                        # print(f'Remaining letters in guess: {future_guess_letters}')
                         future_match = False
                         if guess_letter in future_guess_letters: # double letter in guess, check both
-#                             print('There is another instance of this letter in future guess letters.\
-# Checking if that one is correct.')
                             guess_j = guess_i+1
                             while guess_j < len(guess):
-#                                 print(f'Checking future guess letter {guess[guess_j]} for a match \
-# with future answer letter {answer[guess_j]}')
                                 if guess[guess_j] == answer[guess_j]:
-                                    # print('There is a future match.')
                                     future_match = True
                                     outstr = splice_str(outstr,'O',guess_j)
                                     outstr = splice_str(outstr,'-',guess_i)
-                                    # print(f'outstr now looks like this: {outstr}')
                                     guess = splice_str(guess,'_',guess_j)
                                     answer = splice_str(answer,'_',guess_j)
-                                    # print(f'guess now this: {guess}')
-                                    # print(f'answer now this: {answer}')
                                     action_taken = True
-                                    # guess_i = answer_i = 0
                                     break
                                 guess_j += 1
                             if not future_match:
-                                # print('No future match. Its an X.')
-                                # print(f'inserting X to position {guess_i} in outstr')
                                 outstr = splice_str(outstr,'X',guess_i)
                                 guess = splice_str(guess,'_',guess_i)
                                 answer = splice_str(answer,'_',answer_i)
-                                # print(f'outstr: {outstr}')
                                 action_taken = True
                                 break
                             answer_i += 1
                             continue
                         else:
-                            # print('No repeated letters. Its an X.')
                             outstr = splice_str(outstr,'X',guess_i)
                             action_taken = True
                             break   
@@ -90,7 +68,6 @@ def evaluate_guess(guess,answer):
             answer_i += 1
         
         if not action_taken:
-            # print('Letter not found.')
             outstr = splice_str(outstr,'-',guess_i)
         
         guess_i += 1
@@ -107,11 +84,12 @@ def splice_str(str,letter,index):
     Returns updated str.
     '''
     new_header = str[0:index] + letter
+
     if str[index:]:
         new_footer = str[index+1:]
     else:
         new_footer = ''
-    # print(f'Splicing. New header: {new_header} New footer: {new_footer}')
+
     return new_header + new_footer
 
 def read_wordle(infile):
@@ -142,9 +120,7 @@ and then try again.")
     with open(infile) as f:
         for line in f:
             line_list = line.split()
-            # print(line_list)
             filedate = line_list[0] + ' ' + line_list[1] + ' ' + line_list[2]
-            # print(filedate)
             if filedate == wordle_date:
                 wordle = line_list[-1]
 
